390b9d117ee81ddfa0977e7dbc02910adfb04dcd
hellozts4120 <14307110190@fudan.edu.cn>
2016-04-27 23:38:14 +0800

    add something

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -15,26 +15,18 @@ angular.module('questionnaireApp')
 
     .controller('IndexController', ['$scope', 'formFactory', function($scope, formFactory){
         $scope.forms = formFactory.getForms();
-        
-        $scope.click = function(e) {
-            console.log(e);
-        }
-        
+        $scope.isShow = formFactory.getForms().length > 0;
         $scope.type = ["onboard", "offboard", "ended"];
         $scope.statusHTML = {"onboard": "<p style='color: green'>发布中</p>", "offboard": "<p>未发布</p>", "ended": "<p>已结束</p>"};
         $scope.text = {"onboard": "查看数据", "offboard": "查看问卷", "ended": "查看数据"};
         
-        
-        $scope.init = function () {
-            $("#form-container").hide();
-            $("#new-form").hide();
-            if (formFactory.getForms().length == 0) {
-                $("#new-form").show();
-            }
-            else {
-                $("#form-container").show();
-            }
+        $scope.click = function(e) {
+            var type = e.target.id.split("-")[1];
+            var id = e.target.id.split("-")[2];
+            console.log(type + "  " + id);
         }
+        
+        
         
         
         
