2c31032b348a6d58e29199b5caaaa08ecdc80af2
hellozts4120 <14307110190@fudan.edu.cn>
2016-05-08 13:15:51 +0800

    add gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,86 +1,80 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var sourcemaps = require('gulp-sourcemaps');
-var browserSync = require('browser-sync');
-var useref = require('gulp-useref');
-var uglify = require('gulp-uglify');
-var gulpIf = require('gulp-if');
-var cssnano = require('gulp-cssnano');
-var del = require('del');
-var runSequence = require('run-sequence');
+var gulp = require('gulp'),  
+    minifycss = require('gulp-minify-css'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    clean = require('gulp-clean'),
+    concat = require('gulp-concat'),
+    htmlmin = require('gulp-htmlmin'),
+    cache = require('gulp-cache'),
+    imagemin = require('gulp-imagemin'),
+    modify = require('gulp-modify');
 
-// Development Tasks 
-// -----------------
-
-// Start browserSync server
-gulp.task('browserSync', function() {
-  browserSync({
-    server: {
-      baseDir: 'app'
-    }
-  })
-})
-
-gulp.task('sass', function() {
-  return gulp.src('app/styles/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
-    .pipe(sass()) // Passes it through a gulp-sass
-    .pipe(gulp.dest('app/styles')) // Outputs it in the css folder
-    .pipe(browserSync.reload({ // Reloading with Browser Sync
-      stream: true
-    }));
-})
-
-// Watchers
-gulp.task('watch', function() {
-  gulp.watch('app/styles/**/*.scss', ['sass']);
-  gulp.watch('app/*.html', browserSync.reload);
-  gulp.watch('app/scripts/**/*.js', browserSync.reload);
-})
-
-// Optimization Tasks 
-// ------------------
-
-// Optimizing CSS and JavaScript 
-gulp.task('useref', function() {
-
-  return gulp.src('app/*.html')
-    .pipe(useref())
-    .pipe(gulpIf('*.js', uglify()))
-    .pipe(gulpIf('*.css', cssnano()))
-    .pipe(gulp.dest('dist'));
+gulp.task('js', function(){
+    gulp.src('./app/scripts/*.js')
+    .pipe(concat('QuestionMaker.min.js'))
+    .pipe(uglify({
+        mangle: false,
+        compress: {
+            drop_console: true
+        }
+    }))
+    .pipe(modify({
+        fileModifier: function(file, contents) {
+            return contents.replace(/\/app\//g, '/dist/');
+        }
+    }))
+    .pipe(gulp.dest('./dist/scripts'));
 });
 
-// Copying fonts 
-gulp.task('fonts', function() {
-  return gulp.src('app/fonts/**/*')
-    .pipe(gulp.dest('dist/fonts'))
-})
+gulp.task('fonts', function(){
+    gulp.src('./app/fonts/**/*')
+    .pipe(gulp.dest('./dist/fonts'));
+});
 
-// Cleaning 
+gulp.task('css', function(){
+    gulp.src('./app/styles/**/*.css')
+    .pipe(minifycss())
+    .pipe(gulp.dest('./dist/styles'))
+});
+
+gulp.task('html', function(){
+    gulp.src('./app/views/**/*.html')
+    .pipe(htmlmin({collapseWhitespace: true, minifyJS: true, minifyCss: true}))
+    .pipe(gulp.dest('./dist/views'));
+});
+
+gulp.task('entry', function(){
+    gulp.src('./app/index.html')
+    .pipe(htmlmin({collapseWhitespace: true, minifyJS: true, minifyCss: true}))
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('image', function(){
+    gulp.src('./app/images/*')
+    .pipe(cache(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
+    .pipe(gulp.dest('./dist/images'))
+});
+
 gulp.task('clean', function() {
-  return del.sync('dist').then(function(cb) {
-    return cache.clearAll(cb);
-  });
-})
-
-gulp.task('clean:dist', function() {
-  return del.sync(['dist/**/*', '!dist/images', '!dist/images/**/*']);
+    gulp.src(['./dist/'], {read: false})
+    .pipe(clean({force: true}));
 });
 
-// Build Sequences
-// ---------------
+gulp.task('modify', function(){
+    setTimeout(function(){
+        gulp.src('./dist/index.html')
+        .pipe(modify({
+            fileModifier: function(file, contents) {
+                return contents.replace(/\/app\//g, '/dist/').replace(/<!--\{main\}-->.*<!--\{endmain\}-->/g, '<script src="/dist/js/QuestionMaker.min.js"></scirpt>').replace(/base.css/, 'base.min.css');
+            }
+        }))
+        .pipe(gulp.dest('./dist'));
+    }, 200);
+    
+});
 
-gulp.task('default', function(callback) {
-  runSequence(['sass', 'browserSync', 'watch'],
-    callback
-  )
-})
-
-gulp.task('build', function(callback) {
-  runSequence(
-    'clean:dist',
-    ['sass', 'useref', 'fonts'],
-    callback
-  )
-})
+gulp.task('default', ['clean'], function() {
+    setTimeout(function(){
+        gulp.start(['js', 'image', 'fonts', 'css', 'entry', 'html', 'modify']);
+    }, 100);
+});
